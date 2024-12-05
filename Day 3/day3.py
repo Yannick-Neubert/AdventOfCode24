0e7790cbf94ccac3dd648594d5c7525752c1bee5
Yannick-Neubert <yannick.neubert@gmx.de>
2024-12-06 00:00:48 +0100

    add one-line solutions

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -4,6 +4,10 @@ import re
 with open("Day 3/input.txt", 'r') as file:
     input = file.read()
 
+# One-Liners P1 and P2:
+print("P1: ", sum(list(map(lambda x: int(x[0]) * int(x[1]), [mult[4:-1].split(',') for mult in re.findall("mul\([0-9]{1,3},[0-9]{1,3}\)", re.sub("\n", "", input))]))))
+print("P2: ", sum(list(map(lambda x: int(x[0]) * int(x[1]), [mult[4:-1].split(',') for mult in re.findall("mul\([0-9]{1,3},[0-9]{1,3}\)", re.sub("don't\(\).*?[do\(\)|$]", "", re.sub("\n", "", input)))]))))
+
 # remove any linebreaks
 input = re.sub("\n", "", input)
 
