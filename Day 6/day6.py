140676d9d4827b0d62f845175de0dfc5cf673208
Yannick-Neubert <yannick.neubert@gmx.de>
2024-12-07 10:28:59 +0100

    day 6 done

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,5 +1,6 @@
 #Task: https://adventofcode.com/2024/day/6
 import numpy as np
+from tqdm import tqdm
 
 with open("Day 6/input.txt", 'r') as file:
     input = file.read().strip().split('\n')
@@ -15,11 +16,12 @@ for i, line in enumerate(input):
 input = np.array(input)
 
 # Part 1
-# use tuples for position
-
-
 visited = []
+
+# O(n*m) possible using graphs
+# returns #places visited or -1 if there's a loop
 def check(input):
+    seen = []
     pos = tuple([arr[0] for arr in np.where(input == '^')])
     dx = [0, 1, 0, -1]
     dy = [-1, 0, 1, 0]
@@ -28,28 +30,39 @@ def check(input):
     def add(pos, dir):
         return (pos[0] + dy[dir], pos[1] + dx[dir])
 
-    for i in range(1000):
+    # step by step simulation
+    while True:
         if pos not in visited: 
             visited.append(pos)
-
-        input[pos] = 'X'
 
         ahead = add(pos, dir)
         if input[ahead] == '0': break
         if input[ahead] == '#':
+            pair = (*ahead, dir)
             dir = (dir + 1) % 4
+            if pair in seen:
+                return -1
+            seen.append(pair)
 
+            # may need to turn twice
+            ahead = add(pos, dir)
+            if input[ahead] == '#':
+                dir = (dir + 1) % 4
+            
         pos = add(pos, dir)    
     return(len(visited))
 
 print("Solution part 1: ", check(input))
 
-# for row in input:
-#     print(row)
-
+# Part 2
 # obstacle can't be placed at start
-start, *visited = visited
+start, *visits = visited
+loops = 0
 
-for visit in visited:
+# Optimizable by starting from further into the simulation
+for visit in tqdm(visits):
     input[visit] = '#'
-    break
+    if check(input) == -1:
+        loops += 1
+    input[visit] = '.'
+print("Solution part 2: ", loops)
