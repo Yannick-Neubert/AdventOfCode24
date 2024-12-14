41d01127843dc15b8b39cd890f29f3fe2636ae0e
Yannick-Neubert <yannick.neubert@gmx.de>
2024-12-14 13:05:21 +0100

    return list of edges

--- Day 12/day12.py
+++ Day 12/day12.py
@@ -4,6 +4,8 @@ from collections import deque
 with open("Day 12/input.txt") as file:
     input = np.array([[*line] for line in file.read().strip().split("\n")])
 
+input = np.pad(input, pad_width=1, mode="constant", constant_values='.')
+
 visited = np.zeros(input.shape)
 
 print(input)
@@ -11,40 +13,64 @@ print(input)
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
 
-def neighbours(i, j):
+def neighbours(i, j, return_Edges = False):
     next = []
+    edges = []
     for k in range(len(dx)):
         adjacent = (i + dy[k], j + dx[k])
         if adjacent[0] < 0 or adjacent[0] >= input.shape[0] or adjacent[1] < 0 or adjacent[1] >= input.shape[1]:
             continue
         if input[adjacent] != input[i, j]:
+            edges.append((i, j, k))
             continue
 
         next.append(adjacent)
+    if return_Edges:
+        return next, edges
     return next
 
-def traverse(i, j):
+def traverse(i, j, return_Edges = False):
     if visited[i, j]:
         return 0, 0
     
     visited[i, j] = 1
-    adjacent = neighbours(i, j)
 
+    adjacent = neighbours(i, j, return_Edges)
+    # print(adjacent)
+    edges = []
+    if return_Edges:
+        edges = adjacent[1]
+        adjacent = adjacent[0]
+
+    # print(i,j,edges,adjacent)
+
+    # print(edges)
     next = [x for x in adjacent if not visited[x]]
-    buffer = [0, 0]
+    buffer = [0, 0, []] if return_Edges else [0, 0]
     for x in next:
-        res = traverse(*x)
+        res = traverse(*x, return_Edges)
         buffer[0] += res[0]
         buffer[1] += res[1]
-    ret = buffer[0] + 1, buffer[1] + 4 - len(adjacent)
-    return ret
+        if return_Edges:
+            buffer[2] += res[2]
+    if return_Edges:
+        return buffer[0] + 1, buffer[1] + 4 - len(adjacent), buffer[2] + edges
+    else:
+        return buffer[0] + 1, buffer[1] + 4 - len(adjacent),
+
 
 total = 0
-for i in range(input.shape[0]):
-    for j in range(input.shape[1]):
+for i in range(1, input.shape[0]-1):
+    for j in range(1, input.shape[1]-1):
         if visited[i, j]:
             continue
         ret = traverse(i, j)
         total += ret[0] * ret[1]
 
 print("Solution part 1: ", total)
+
+
+visited = np.zeros(input.shape)
+
+# print(neighbours(1, 1, True))
+print(traverse(1, 1, return_Edges=True))
