716f080cdf5d28023e9e9dd8fff8dc2ebd304844
Yannick-Neubert <yannick.neubert@gmx.de>
2024-12-14 22:11:25 +0100

    day 12 done

--- Day 12/day12.py
+++ Day 12/day12.py
@@ -6,13 +6,14 @@ with open("Day 12/input.txt") as file:
 
 input = np.pad(input, pad_width=1, mode="constant", constant_values='.')
 
+# Part 1
 visited = np.zeros(input.shape)
 
-print(input)
-
+# directions
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
 
+# get adjacent cells of same type and borders
 def neighbours(i, j, return_Edges = False):
     next = []
     edges = []
@@ -29,36 +30,33 @@ def neighbours(i, j, return_Edges = False):
         return next, edges
     return next
 
-def traverse(i, j, return_Edges = False):
+# traverse a field and return area, perimeter, borders
+def traverse(i, j, return_edges = False):
     if visited[i, j]:
-        return 0, 0
+        return 0, 0, [] if return_edges else 0, 0
     
     visited[i, j] = 1
 
-    adjacent = neighbours(i, j, return_Edges)
-    # print(adjacent)
+    adjacent = neighbours(i, j, return_edges)
     edges = []
-    if return_Edges:
+    if return_edges:
         edges = adjacent[1]
         adjacent = adjacent[0]
 
-    # print(i,j,edges,adjacent)
-
-    # print(edges)
     next = [x for x in adjacent if not visited[x]]
-    buffer = [0, 0, []] if return_Edges else [0, 0]
+    buffer = [0, 0, []] if return_edges else [0, 0]
     for x in next:
-        res = traverse(*x, return_Edges)
+        res = traverse(*x, return_edges)
         buffer[0] += res[0]
         buffer[1] += res[1]
-        if return_Edges:
+        if return_edges:
             buffer[2] += res[2]
-    if return_Edges:
+    if return_edges:
         return buffer[0] + 1, buffer[1] + 4 - len(adjacent), buffer[2] + edges
     else:
         return buffer[0] + 1, buffer[1] + 4 - len(adjacent),
 
-
+# traverse each field
 total = 0
 for i in range(1, input.shape[0]-1):
     for j in range(1, input.shape[1]-1):
@@ -69,8 +67,32 @@ for i in range(1, input.shape[0]-1):
 
 print("Solution part 1: ", total)
 
+# Part 2
+# number of sides = number of corners
+# a horizontal side is a corner if theres no horizontal edge left/right of it
+# all corners have a horizontal edge
+def count_corners(edges):
+    corners = 0 
+    for edge in sorted(edges):
+        if (edge[0], edge[1] + 1) not in edges:
+            corners += 1
+        if (edge[0], edge[1] - 1) not in edges:
+            corners += 1
+    return corners
 
 visited = np.zeros(input.shape)
 
-# print(neighbours(1, 1, True))
-print(traverse(1, 1, return_Edges=True))
+# top and bottom edges need to be counted seperately
+total = 0
+for i in range(1, input.shape[0]-1):
+    for j in range(1, input.shape[1]-1):
+        if visited[i, j]:
+            continue
+        ret = traverse(i, j, return_edges=True)
+        edges = ret[2]
+        top_edges = [(edge[0], edge[1]) for edge in edges if edge[2] == 0]
+        bottom_edges = [(edge[0], edge[1]) for edge in edges if edge[2] == 2]
+
+        total += ret[0] * (count_corners(top_edges) + count_corners(bottom_edges))
+
+print("Solution part 2: ", total)
